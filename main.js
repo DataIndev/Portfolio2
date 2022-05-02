498981e419cd4c962e76323cefe29d97c7a0545b
DataIndev <admin@dataindev.com>
2022-05-02 15:50:59 -0300

    .

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+  
+})
